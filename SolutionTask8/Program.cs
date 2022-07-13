625234900be313d776ede77e58baa7704126a3d2
Andrey G <gav27@mail.ru>
2022-07-13 13:17:37 +0500

    Add solution task 8

---
+++ SolutionTask8/Program.cs
@@ -0,0 +1,11 @@
+﻿string? number = Console.ReadLine();
+int n = int.Parse(number);
+Console.WriteLine(n);
+int i = 1;
+while (n > 1)
+{
+    int result = i * 2;
+    n = n - 2;
+    i++;
+    Console.Write(result + " ");
+}
