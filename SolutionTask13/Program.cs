9262c7093d696403dfc2d1f533c98bf38e3b7eb9
Andrey G <gav27@mail.ru>
2022-07-14 12:45:02 +0500

    Add solution task 13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,17 @@
+﻿m1: Console.Write("Введите число:");
+string? number = Console.ReadLine();
+if (number == "")
+{
+    goto m1;
+}
+int num = int.Parse(number);
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    char[] res = number.ToCharArray();
+    Console.WriteLine(res[2]);
+}
+
