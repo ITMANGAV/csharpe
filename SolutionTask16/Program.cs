fc4c55d31a30df0edea859e9a0e90662d7d471d4
Andrey G <gav27@mail.ru>
2022-07-14 19:12:29 +0500

    ADD solution task 16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,13 @@
+﻿string? number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+string? number2 = Console.ReadLine();
+int num2 = int.Parse(number2);
+
+if (num1 == num2 * num2 || num2 == num1 * num1)
+{
+    Console.WriteLine(num1 + " " + num2 + " Да");
+}
+else
+{
+    Console.WriteLine(num1 + " " + num2 + " Нет");
+}
