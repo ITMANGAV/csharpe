a497460fa26b3c25d23714ae4de1965c845dede7
Andrey G <gav27@mail.ru>
2022-07-14 12:57:31 +0500

    add solution task 15

---
+++ SolutionTask15/Program.cs
@@ -0,0 +1,13 @@
+﻿m1: Console.Write("Введите число, обозначавшее день недели:");
+string? number = Console.ReadLine();
+if (number == "") { goto m1; }
+int num = int.Parse(number);
+if (num < 1 || num > 7) { Console.WriteLine("Число не коректное, попробуйте ещё раз!"); goto m1; }
+if (num > 5)
+{
+    Console.WriteLine(num + " -> Да, выходной.");
+}
+else
+{
+    Console.WriteLine(num + " -> Нет, рабочий.");
+}
