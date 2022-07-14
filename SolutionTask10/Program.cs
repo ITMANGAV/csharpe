8be7737bc27147fe8fd625a11ca520ae37e3ecf4
Andrey G <gav27@mail.ru>
2022-07-14 12:30:30 +0500

    add solution task 10

---
+++ SolutionTask10/Program.cs
@@ -0,0 +1,11 @@
+﻿m1: Console.Write("Введите трехзначное число:");
+string? number = Console.ReadLine();
+int num = int.Parse(number);
+if ((num < 100) || (num > 999))
+{
+    Console.WriteLine("НЕ трехзначное число!");
+    goto m1;
+}
+int res = num / 10 % 10;
+Console.WriteLine(res);
+
