8e59f2e66fa8068c52145e54d82d49d7b5212316
Andrey G <gav27@mail.ru>
2022-07-19 21:06:21 +0500

    Add solutions tasks 19+ and 21+

---
+++ SolutionTask19/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите пятизначное число:");
+string? number = Console.ReadLine();
+int num = int.Parse(number);
+if ((num / 10000 > 9) || (num / 10000 == 0))
+{
+    Console.Write("Введенное число не пятизначное!");
+}
+else
+{
+    int n1 = num / 10000;
+    int n2 = (num - n1 * 10000) / 1000;
+    int n3 = (num - n1 * 10000 - n2 * 1000) / 100;
+    int n4 = (num - n1 * 10000 - n2 * 1000 - n3 * 100) / 10;
+    int n5 = num % 10;
+    int digit = n1 * 1000 + n2 * 100 + n4 * 10 + n5;
+    //Console.WriteLine($"{n1} {n2} {n3} {n4} {n5}");
+    Console.WriteLine(digit);
+    var polindrom = new Dictionary<int, int>();
+    int k = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        for (int j = 1; j < 10; j++)
+        {
+            polindrom[k] = i * 1000 + j * 100 + j * 10 + i;
+            k++;
+        }
+    }
+    int p = 0;
+    for (int i = 1; i < k; i++)
+    {
+        if (digit == polindrom[i])
+        {
+            p = 1;
+        }
+    }
+    if (p == 1)
+    {
+        Console.Write("Введённое число палиндром!");
+    }
+    else
+    {
+        Console.Write("Введённое число не палиндром!");
+    }
+}
---
+++ SolutionTask21/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите данные точек в формате A(3,6,8);B(2,1,-7)");
+string? str = Console.ReadLine();
+string a = str.Substring(0, str.IndexOf(";"));
+string b = str.Substring((str.IndexOf(";") + 1));
+
+string a1 = a.Substring((str.IndexOf("(") + 1), (str.IndexOf(",") - str.IndexOf("(") - 1));
+string bak = a.Substring((str.IndexOf(",") + 1));
+string a2 = bak.Substring(0, bak.IndexOf(","));
+string bak1 = bak.Substring((bak.IndexOf(",") + 1));
+//Console.WriteLine($"bak={bak} bak1={bak1}");
+string a3 = bak1.Substring(0, bak1.IndexOf(")"));
+
+string b1 = b.Substring((b.IndexOf("(") + 1), (b.IndexOf(",") - b.IndexOf("(") - 1));
+string bakb = b.Substring((b.IndexOf(",") + 1));
+string b2 = bakb.Substring(0, bakb.IndexOf(","));
+string bakb1 = bakb.Substring((bakb.IndexOf(",") + 1));
+//Console.WriteLine($"bakb={bakb} bakb1={bakb1}");
+string b3 = bakb1.Substring(0, bakb1.IndexOf(")"));
+
+int x1 = int.Parse(a1);
+int y1 = int.Parse(a2);
+int z1 = int.Parse(a3);
+
+int x2 = int.Parse(b1);
+int y2 = int.Parse(b2);
+int z2 = int.Parse(b3);
+
+double rezultat = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
+Console.WriteLine($"Расстояние между {a} и  {b} -> {rezultat}");
+//Console.WriteLine($"{a1} | {a2} | {a3}  ;  {b1} | {b2} | {b3}");
