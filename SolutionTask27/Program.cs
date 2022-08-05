ef1de3d9664348f43eb7758c3ff83649e1b566de
Andrey G <gav27@mail.ru>
2022-08-05 16:32:21 +0500

    Add solutions task 27* and task 29*

---
+++ SolutionTask27/Program.cs
@@ -0,0 +1,40 @@
+﻿string readStr()
+{
+    Console.Write("Введите число: ");
+    string? str = Console.ReadLine();
+    return str;
+}
+string stroka = readStr();
+void solutionChar(string stroka)
+{
+    char[] arr = stroka.ToCharArray();
+    int[] Aint = Array.ConvertAll(arr, c => (int)Char.GetNumericValue(c));
+    int sum = 0;
+    for (int i = 0; i < Aint.Length; i++)
+    {
+        sum += Aint[i];
+    }
+    Console.WriteLine($"Сумма цифр числа {stroka} -> {sum}");
+
+}
+void solutionSimple(string stroka)
+{
+    int digit = int.Parse(stroka);
+    int d = 1;
+    int e = 1;
+    int sum = 0;
+    while (d > 0 && e > 0)
+    {
+        d = digit / 10;
+        e = digit % 10;
+        sum += e;
+        digit = d;
+    }
+    Console.WriteLine($"Сумма цифр числа {stroka} -> {sum}");
+}
+DateTime t = DateTime.Now;
+solutionChar(stroka);
+Console.WriteLine(DateTime.Now - t);
+t = DateTime.Now;
+solutionSimple(stroka);
+Console.WriteLine(DateTime.Now - t);
---
+++ SolutionTask29/Program.cs
@@ -0,0 +1,17 @@
+﻿string readStr()
+{
+    Console.Write("Введите имена: ");
+    string? str = Console.ReadLine();
+    return str;
+}
+
+void solution(string stroka)
+{
+    string[] names = stroka.Split(new string[] { "," }, StringSplitOptions.None);
+    int count = names.Length;
+    int number = new Random().Next(0, count);
+    Console.WriteLine($"Выиграл -> {names[number]}");
+}
+
+string stroka = readStr();
+solution(stroka);
