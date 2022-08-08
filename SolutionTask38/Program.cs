7d0f286ece8f216381619550db0286ae0cca8352
Andrey G <gav27@mail.ru>
2022-08-08 21:20:02 +0500

    Add solution task 38*

---
+++ SolutionTask38/Program.cs
@@ -0,0 +1,46 @@
+﻿double[] CreateArray()
+{
+    Console.Write("Введите число, размер массива: ");
+    string? number = Console.ReadLine();
+    int num = int.Parse(number);
+    double[] arr = new double[num];
+    Random random = new Random();
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = random.NextDouble() * 100;
+    }
+    return arr;
+}
+
+void PrintArr(double[] arr1)
+{
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        Console.Write("{0,6:F3} ", arr1[i]);
+    }
+}
+
+double[] InsertionSort(double[] array)
+{
+    double[] result = new double[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int j = i;
+        while (j > 0 && result[j - 1] > array[i])
+        {
+            result[j] = result[j - 1];
+            j--;
+        }
+        result[j] = array[i];
+    }
+    return result;
+}
+
+double[] newArray = CreateArray();
+PrintArr(newArray);
+double[] sortArray = InsertionSort(newArray);
+Console.WriteLine();
+PrintArr(sortArray);
+double rez = sortArray[sortArray.Length - 1] - sortArray[0];
+Console.WriteLine();
+Console.WriteLine($"Разница между мин и мак значениями = {rez}");
