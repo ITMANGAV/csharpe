66097a7f87497b0e93147fa5b24b8dcea5054fd3
Andrey G <gav27@mail.ru>
2022-08-13 19:03:55 +0500

    Add solution task 41

---
+++ SolutionTask41/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите последовательность чисел через запятую: ");
+string? str = Console.ReadLine();
+string[] data = str.Split(new string[] { "," }, StringSplitOptions.None);
+int[] digits = new int[data.Length];
+int i = 0;
+foreach (string k in data)
+{
+    digits[i] = int.Parse(k);
+    //Console.WriteLine(digits[i]);
+    i++;
+}
+
+int More0(int[] arr)
+{
+    int p = 0;
+    foreach (int j in arr)
+    {
+        if (j > 0) { p++; }
+    }
+    return p;
+}
+
+Console.WriteLine(More0(digits));
