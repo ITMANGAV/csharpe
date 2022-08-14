632e616789b2cb8fb27401acebc0666f39c15de4
Andrey G <gav27@mail.ru>
2022-08-14 19:41:20 +0500

    Add solution task 47

---
+++ SolutionTask47/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите количество строк 2х мерного массива: ");
+int row = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите количество столбцов 2х мерного массива: ");
+int col = int.Parse(Console.ReadLine() ?? "");
+
+
+double[,] CreateArray(int row, int col)
+{
+    double[,] outArray = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            outArray[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return outArray;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write("{0,6:F2} ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+double[,] oArr = CreateArray(row, col);
+PrintArray(oArr);
