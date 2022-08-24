789c6b114cae8ac331ed896ce7b43fa427df624f
Andrey G <gav27@mail.ru>
2022-08-24 17:32:54 +0500

    Add solution task 68

---
+++ SolutionTask68/Program.cs
@@ -0,0 +1,26 @@
+﻿int ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return ack(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return ack(m - 1, ack(m, n - 1));
+    }
+    else
+        return n + 1;
+}
+int ReadData(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+int m = ReadData("Введите натуральное число M: ");
+int n = ReadData("Введите натуральное число N: ");
+Console.WriteLine(ack(m, n));
