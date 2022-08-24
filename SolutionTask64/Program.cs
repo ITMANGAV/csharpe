0ba9687f4868b84a3e694eac80aa9fb500b1ff53
Andrey G <gav27@mail.ru>
2022-08-24 17:05:57 +0500

    Add solution task 64

---
+++ SolutionTask64/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadData(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+//Метод печатает натуральныу числа от m до n
+void Natnumbers(int m, int n)
+{
+    if (m == n + 1) { return; }
+    Console.Write(m + " ");
+    Natnumbers(++m, n);
+
+}
+
+int m = ReadData("Введите натуральное число M, начало отсчета: ");
+int n = ReadData("Введите натуральное число N, конец отсчета: ");
+Natnumbers(m, n);
