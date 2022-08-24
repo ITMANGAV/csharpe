4bb399bed759eaa0f9f7f02edae343a9ab5d2b7a
Andrey G <gav27@mail.ru>
2022-08-24 17:06:24 +0500

    Add solution task 66

---
+++ SolutionTask66/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadData(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+int sum = 0;
+//Метод сумирует натуральные числа от m до n
+int SumNatNumbers(int m, int n)
+{
+    if (m == n + 1) { return sum; }
+    sum += m;
+    SumNatNumbers(++m, n);
+    return sum;
+}
+
+int m = ReadData("Введите натуральное число M, начало отсчета: ");
+int n = ReadData("Введите натуральное число N, конец отсчета: ");
+int otvet = SumNatNumbers(m, n);
+Console.WriteLine($"Сумма = {otvet}");
